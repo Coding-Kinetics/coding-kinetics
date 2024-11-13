427f211ed74090fea13dd49bc25e7d32144a8805
ahinchman1 <ahinchman1@gmail.com>
2024-11-12 23:16:08 -0600

    Added sliding toggle for instructions on roadmap content, cleaned up hamburger menu

--- js/get-started.js
+++ js/get-started.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+    // select novice dev
+    $("#novice-dev").click(function() {
+        // add selection to current element
+        $("#novice-dev").addClass("active");
+
+        // remove selection from other choices
+        $("#java-dev").removeClass("active");
+        $("#team").removeClass("active");
+        $("#kotlin-dev").removeClass("active");
+
+        // show single player roadmap
+        $(".roadmap-single").show();
+        $(".roadmap-team").hide();
+    });
+
     // select java dev
     $("#java-dev").click(function() {
         // add selection to current element
@@ -11,8 +26,38 @@ $(document).ready(function() {
         $("#novice-dev").removeClass("active");
 
         // show single player roadmap
-        $(".roadmap-single").show();
+        $(".roadmap-single").slideToggle();
         $(".roadmap-team").hide();
+    });
+
+    // select kotlin dev
+    $("#kotlin-dev").click(function() {
+        // add selection to current element
+        $("#kotlin-dev").addClass("active");
+
+        // remove selection from other choices
+        $("#java-dev").removeClass("active");
+        $("#team").removeClass("active");
+        $("#novice-dev").removeClass("active");
+
+        // show single player roadmap
+        $(".roadmap-single").slideToggle();
+        $(".roadmap-team").hide();
+    });
+
+    // select team
+    $("#team").click(function() {
+        // add selection to current element
+        $("#team").addClass("active");
+
+        // remove selection from other choices
+        $("#java-dev").removeClass("active");
+        $("#kotlin-dev").removeClass("active");
+        $("#novice-dev").removeClass("active");
+
+        // show single player roadmap
+        $(".roadmap-single").hide();
+        $(".roadmap-team").slideToggle();
     });
 });
 
