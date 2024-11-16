a74a65eb3fa60e2e30292aa811127a66793bdcd0
ahinchman1 <ahinchman1@gmail.com>
2024-11-16 15:54:41 -0600

    Moved navigation css to global.css, and added some functionality for mobile mode as well as making the header fixed with some shadowing

--- js/get-started.js
+++ js/get-started.js
@@ -8,7 +8,9 @@ $(document).ready(function() {
         // remove selection from other choices
         $("#java-dev").removeClass("active");
         $("#team").removeClass("active");
+
         $("#kotlin-dev").removeClass("active");
+        $(".kotlin-dev-img").attr('src','./assets/kotlin.png');
 
         // show single player roadmap
         $(".roadmap-single").show();
@@ -22,6 +24,7 @@ $(document).ready(function() {
 
         // remove selection from other choices
         $("#kotlin-dev").removeClass("active");
+        $(".kotlin-dev-img").attr('src','./assets/kotlin.png');
         $("#team").removeClass("active");
         $("#novice-dev").removeClass("active");
 
@@ -31,9 +34,13 @@ $(document).ready(function() {
     });
 
     // select kotlin dev
-    $("#kotlin-dev").click(function() {
+    $("#kotlin-dev").hover(
+        function() { if (!$(this).hasClass("active")) $(".kotlin-dev-img").attr('src','./assets/kotlin-dev.png'); },
+        function() { if (!$(this).hasClass("active")) $(".kotlin-dev-img").attr('src','./assets/kotlin.png'); },
+    ).click(function() {
         // add selection to current element
         $("#kotlin-dev").addClass("active");
+        $(".kotlin-dev-img.active").attr('src','./assets/kotlin-dev.png');
 
         // remove selection from other choices
         $("#java-dev").removeClass("active");
@@ -53,6 +60,7 @@ $(document).ready(function() {
         // remove selection from other choices
         $("#java-dev").removeClass("active");
         $("#kotlin-dev").removeClass("active");
+        $(".kotlin-dev-img").attr('src','./assets/kotlin.png');
         $("#novice-dev").removeClass("active");
 
         // show single player roadmap
--- js/global.js
+++ js/global.js
@@ -1,4 +1,23 @@
+
+/** MailerLite Universal **/
+
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1179815');
+
+/** end MailerLite Universal **/
+
 $(document).ready(function() {
+
+    var cookieCount = document.cookie.split('; ').findIndex(x => x.split('=')[0] == 'cookiename');
+    if(cookieCount >= 0) {
+        // some code
+    } else {
+        window["ga-disable-UA-7358061-1"] = true;
+        window["gid-disable-UA-7358061-1"] = true;
+    }
 
     // subscribe screen pops up
     $(".email-subscribe-button").click(function() {
@@ -11,19 +30,16 @@ $(document).ready(function() {
     });
 
     $(document).on('keydown', function() {
-          if (event.key == "Escape" && $("#subscribe-screen").is(":visible")) {
+        if ($("#subscribe-screen").is(":visible") && event.key == "Escape") {
             $("#subscribe-screen").hide();
-       }
+        }
+    });
+
+    $(".icon").click(function() {
+        $(".topnav").addClass("topnav-selected");
+        $(".nav-links").addClass("selected")
     });
 });
-
-// MailerLite Universal 
-
-(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-    ml('account', '1179815');
 
 
 
