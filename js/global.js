70d0f510b1d3702962ebca866b5081eadde9d0dc
ahinchman1 <ahinchman1@gmail.com>
2024-11-15 18:29:15 -0600

    added functionality for signing up for product updates as a popup/closeout, fixed styling for products page but still need to finish tweaking the content itself

--- js/global.js
+++ js/global.js
@@ -1,3 +1,22 @@
+$(document).ready(function() {
+
+    // subscribe screen pops up
+    $(".email-subscribe-button").click(function() {
+        // show single player roadmap
+        $("#subscribe-screen").show();
+    });
+
+    $("#subscribe-screen").click(function() {
+        $("#subscribe-screen").hide();
+    });
+
+    $(document).on('keydown', function() {
+          if (event.key == "Escape" && $("#subscribe-screen").is(":visible")) {
+            $("#subscribe-screen").hide();
+       }
+    });
+});
+
 // MailerLite Universal 
 
 (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
@@ -5,4 +24,6 @@
     n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
     (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
     ml('account', '1179815');
+
+
 
