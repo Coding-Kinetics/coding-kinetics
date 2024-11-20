0aac471d9ac10db7baf0a8cda7c959cd5c253d8c
ahinchman1 <ahinchman1@gmail.com>
2024-11-20 16:23:31 -0600

    More work on the products page to include details of workshops, and started organizing media css to make sure phone version matches

--- js/get-started.js
+++ js/get-started.js
@@ -1,66 +1,100 @@
 $(document).ready(function() {
 
-    // select novice dev
-    $("#novice-dev").click(function() {
+    /**
+     * Select novice dev
+     **/
+
+    $("#novice-dev").hover(
+        function() { if (!$(this).hasClass("active")) $(".novice-dev-img").attr('src','./assets/novice-dev.png'); },
+        function() { if (!$(this).hasClass("active")) $(".novice-dev-img").attr('src','./assets/novice.png'); },
+    ).click(function() {
         // add selection to current element
         $("#novice-dev").addClass("active");
+        $(".novice-dev-img.active").attr('src','./assets/novice-dev.png');
 
         // remove selection from other choices
         $("#java-dev").removeClass("active");
+        $(".java-dev-img").attr('src','./assets/java.png');
+
         $("#team").removeClass("active");
 
         $("#kotlin-dev").removeClass("active");
         $(".kotlin-dev-img").attr('src','./assets/kotlin.png');
 
         // show single player roadmap
-        $(".roadmap-single").show();
+        if (!$(".roadmap-single").is(":visible")) $(".roadmap-single").show();
         $(".roadmap-team").hide();
     });
 
-    // select java dev
-    $("#java-dev").click(function() {
+    /**
+     * Select java dev
+     **/
+
+    $("#java-dev").hover(
+        function() { if (!$(this).hasClass("active")) $(".java-dev-img").attr('src','./assets/java-dev.png'); },
+        function() { if (!$(this).hasClass("active")) $(".java-dev-img").attr('src','./assets/java.png'); },
+    ).click(function() {
+
         // add selection to current element
         $("#java-dev").addClass("active");
+        $(".java-dev-img").attr('src','./assets/java-dev.png');
 
         // remove selection from other choices
         $("#kotlin-dev").removeClass("active");
         $(".kotlin-dev-img").attr('src','./assets/kotlin.png');
+
         $("#team").removeClass("active");
+
         $("#novice-dev").removeClass("active");
+        $(".novice-dev-img").attr('src','./assets/novice.png');
 
         // show single player roadmap
-        $(".roadmap-single").slideToggle();
+        if (!$(".roadmap-single").is(":visible")) $(".roadmap-single").slideToggle();
         $(".roadmap-team").hide();
     });
 
-    // select kotlin dev
+    /**
+     * Select kotlin dev
+     **/
+
     $("#kotlin-dev").hover(
         function() { if (!$(this).hasClass("active")) $(".kotlin-dev-img").attr('src','./assets/kotlin-dev.png'); },
         function() { if (!$(this).hasClass("active")) $(".kotlin-dev-img").attr('src','./assets/kotlin.png'); },
     ).click(function() {
+
         // add selection to current element
         $("#kotlin-dev").addClass("active");
         $(".kotlin-dev-img.active").attr('src','./assets/kotlin-dev.png');
 
         // remove selection from other choices
         $("#java-dev").removeClass("active");
+        $(".java-dev-img").attr('src','./assets/java.png');
+
         $("#team").removeClass("active");
+
         $("#novice-dev").removeClass("active");
+        $(".novice-dev-img").attr('src','./assets/novice.png');
 
         // show single player roadmap
-        $(".roadmap-single").slideToggle();
+        if (!$(".roadmap-single").is(":visible")) $(".roadmap-single").slideToggle();
         $(".roadmap-team").hide();
     });
 
-    // select team
+    /**
+     * Select team
+     **/
+
     $("#team").click(function() {
         // add selection to current element
         $("#team").addClass("active");
 
         // remove selection from other choices
         $("#java-dev").removeClass("active");
+        $(".java-dev-img.active").attr('src','./assets/java.png');
+
         $("#kotlin-dev").removeClass("active");
         $(".kotlin-dev-img").attr('src','./assets/kotlin.png');
+
         $("#novice-dev").removeClass("active");
 
         // show single player roadmap
